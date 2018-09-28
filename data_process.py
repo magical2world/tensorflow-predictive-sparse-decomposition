1f89a5ca34e3bd1c738f21d57e00c66fc235a426
magical2world <13390100342@163.com>
2018-09-28 21:34:32 +0800

    09-28

--- data_process.py
+++ data_process.py
@@ -6,17 +6,41 @@ from psd import *
 batch_size=128
 image=tf.placeholder(tf.float32,[batch_size,784])
 
-F=encoder(image,256)
-Z=tf.Variable(tf.zeros(256))
-Z=tf.tile(Z,[batch_size,1])
-print(Z)
-B=tf.Variable(tf.random_normal([256,784],stddev=0.01))
-total_loss=loss(image,Z,F,B,1,1)
-optimizer=tf.train.AdamOptimizer(0.001).minimize(total_loss)
+with tf.variable_scope('step1'):
+    F=encoder(image,256)
+with tf.variable_scope('step2'):
+    Z=tf.Variable(tf.random_normal([batch_size,256],stddev=0.01))
+with tf.variable_scope('step1'):
+    B=tf.Variable(tf.random_normal([256,784],stddev=0.01))
+total_loss=loss(image,Z,F,B,0.5,1)
+vars=tf.trainable_variables()
+step1_vars=[var for var in vars if 'step1' in var.name]
+step2_vars=[var for var in vars if 'step2' in var.name]
+restore_img=tf.matmul(Z,B)
+global_step=tf.Variable(0, trainable=False,name='global_step',dtype=tf.int64)
+lr=tf.train.exponential_decay(0.0001,global_step,decay_steps=100,decay_rate=0.95,staircase=True)
+optimizer1=tf.train.AdamOptimizer(lr).minimize(total_loss,var_list=step1_vars,global_step=global_step)
+optimizer2=tf.train.AdamOptimizer(lr).minimize(total_loss,var_list=step2_vars,global_step=global_step)
 
 with tf.Session() as sess:
-    for i in range(100):
+    import numpy as np
+    sess.run(tf.global_variables_initializer())
+    for i in range(1000):
         xs,ys=mnist.train.next_batch(batch_size)
-        l,_=sess.run([total_loss,optimizer],feed_dict={image:xs})
+        xs=(xs-0.5)*2
+        l1,_=sess.run([total_loss,optimizer1],feed_dict={image:xs})
+        l1,_=sess.run([total_loss,optimizer1],feed_dict={image:xs})
+        l1,_=sess.run([total_loss,optimizer1],feed_dict={image:xs})
+        l1, _ = sess.run([total_loss, optimizer1], feed_dict={image: xs})
+        l2,_=sess.run([total_loss,optimizer2],feed_dict={image:xs})
+        print('loss 1 is %f'%(l1))
+        print('loss 2 is %f'%(l2))
+        img=sess.run(restore_img,feed_dict={image:xs})
+    import matplotlib.pyplot as plt
+    plt.subplot(121)
+    plt.imshow(((img[1]+0.5)/2).reshape([28,28]))
+    plt.subplot(122)
+    plt.imshow(((xs[1]+0.5)/2).reshape([28,28]))
+    plt.show()
 
     # print(xs.shape)
--- psd.py
+++ psd.py
@@ -3,14 +3,14 @@ import tensorflow as tf
 def encoder(Y,encoder_size,scope='encoder'):
     with tf.variable_scope(scope):
         G=tf.Variable(tf.random_normal(shape=[encoder_size,encoder_size],stddev=0.01))
-        W=tf.Variable(tf.random_normal(shape=[encoder_size,Y.shape[1].value],stddev=0.01))
+        W=tf.Variable(tf.random_normal(shape=[Y.shape[1].value,encoder_size],stddev=0.01))
         D=tf.Variable(tf.zeros(encoder_size))
-        return tf.matmul(G,tf.nn.tanh(tf.add(tf.multiply(W,Y),D)))
+        return tf.matmul(tf.nn.tanh(tf.add(tf.matmul(Y,W),D)),G)
 
 def loss(Y,Z,F,B,alpha,phi,scope='loss'):
-    loss1=tf.nn.l2_loss(Y,tf.matmul(B,Z))
-    loss2=tf.abs(Z)
-    loss3=tf.nn.l2_loss(Z,F)
+    loss1=tf.sqrt(tf.nn.l2_loss(tf.subtract(Y,tf.matmul(Z,B))))
+    loss2=tf.reduce_mean(tf.abs(Z),axis=-1)
+    loss3=tf.sqrt(tf.nn.l2_loss(tf.subtract(Z,F)))
     with tf.variable_scope(scope):
         loss=loss1+phi*loss2+alpha*loss3
     return tf.reduce_mean(loss)
